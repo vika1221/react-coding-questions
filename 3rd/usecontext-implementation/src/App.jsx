0e3823d212f1dd1f9d9096306da69a9cd3622a60
Vikash <vikashranjanofficial@gmail.com>
2024-04-22 16:50:28 +0530

    multiple values in the context

--- 3rd/usecontext-implementation/src/App.jsx
+++ 3rd/usecontext-implementation/src/App.jsx
@@ -3,21 +3,30 @@ import Child1 from "./Child1";
 import { createContext } from "react";
 
 const myContext = createContext();
+const myContext2 = createContext();
 
 function App() {
   const data1 = "Hello from your parent";
-  const data2 = "Hello bachho... :)"
-  const data3 ={data1, data2};
+  const data2 = "Hello bachho... :)";
+  const data3 = { data1, data2 };
 
   return (
     <>
-      <myContext.Provider value={data3} >
+      {/* for multiple values */}
+      <myContext.Provider value={data1}>
+        <myContext2.Provider value={data2}>
+          <h1>useContext Implementation</h1>
+          <Child1 />
+        </myContext2.Provider>
+      </myContext.Provider>
+
+      {/* <myContext.Provider value={data3} >
         <h1>useContext Implementation</h1>
         <Child1 />
-      </myContext.Provider>
+      </myContext.Provider> */}
     </>
   );
 }
 
-export {myContext};
+export { myContext,myContext2 };
 export default App;
--- 3rd/usecontext-implementation/src/Child3.jsx
+++ 3rd/usecontext-implementation/src/Child3.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { useContext } from 'react'
-import { myContext } from './App'
+import { myContext, myContext2 } from './App'
 
 export default function Child3() {
     const data1 = useContext(myContext);
-    // const data2 = useContext(myContext);
+    const data2 = useContext(myContext2);
     console.log(data1);
   return (
     <div>
       <h3>Child 3 component</h3>
-      <h2>Message from parent component : {data1.data1}</h2>
-      <h2>More Messages from parent component : {data1.data2}</h2>
+      <h2>Message from parent component : {data1}</h2>
+      <h2>More Messages from parent component : {data2}</h2>
     </div>
   )
 }
